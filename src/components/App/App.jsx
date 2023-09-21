f1127d1c385b88782de3f7543e5b851cdf009c13
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-09-21 17:25:06 +0300

    reducer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import Section from 'components/Section/Section';
 import Notification from 'components/Notification/Notification';
 import { Container } from './App.styled';
 
+function reducer(state, action) {
+  if (action.type === 'incremented_good') {
+    return { ...state, good: state.good + 1 };
+  }
+  if (action.type === 'incremented_neutral') {
+    return { ...state, neutral: state.neutral + 1 };
+  }
+  if (action.type === 'incremented_bad') {
+    return { ...state, bad: state.bad + 1 };
+  }
+  if (action.type === 'incremented_total') {
+    return { ...state, totalFeedback: state.good + state.neutral + state.bad };
+  }
+  if (action.type === 'countPositiveFeedback') {
+    return {
+      ...state,
+      countPositiveFeedback: Math.round(
+        (state.good / state.totalFeedback) * 100
+      ),
+    };
+  }
+}
+
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [totalFeedback, setTotalFeedback] = useState(0);
-  const [countPositiveFeedback, setCountPositiveFeedback] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    totalFeedback: 0,
+    countPositiveFeedback: 0,
+  });
+
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+  // const [totalFeedback, setTotalFeedback] = useState(0);
+  // const [countPositiveFeedback, setCountPositiveFeedback] = useState(0);
 
   const onLeaveFeedback = btnName => {
     if (btnName === 'good') {
-      setGood(prevState => prevState + 1);
+      dispatch({ type: 'incremented_good' });
       return;
     }
     if (btnName === 'neutral') {
-      setNeutral(prevState => prevState + 1);
+      dispatch({ type: 'incremented_neutral' });
       return;
     }
     if (btnName === 'bad') {
-      setBad(prevState => prevState + 1);
+      dispatch({ type: 'incremented_bad' });
       return;
     } else {
       console.log('error');
@@ -30,12 +61,12 @@ export const App = () => {
   };
 
   useEffect(() => {
-    setTotalFeedback(good + neutral + bad);
-  }, [good, neutral, bad]);
+    dispatch({ type: 'incremented_total' });
+  }, [state.good, state.neutral, state.bad]);
 
   useEffect(() => {
-    setCountPositiveFeedback(Math.round((good / totalFeedback) * 100));
-  }, [totalFeedback]);
+    dispatch({ type: 'countPositiveFeedback' });
+  }, [state.totalFeedback]);
 
   return (
     <Container>
@@ -47,13 +78,13 @@ export const App = () => {
       </Section>
 
       <Section title="Statistics">
-        {totalFeedback !== 0 ? (
+        {state.totalFeedback !== 0 ? (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={countPositiveFeedback}
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={state.totalFeedback}
+            positivePercentage={state.countPositiveFeedback}
           />
         ) : (
           <Notification message="There is no feedback" />
